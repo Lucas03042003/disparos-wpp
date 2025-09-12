755fb0c8c0d3fa9586c8b4370cfbfda31e6cec95
Lucas03042003 <lucasbrcoelho@gmail.com>
2025-09-12 14:48:49 -0300

    Chore: Setup Better-Auth tables in my database.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,92 @@
 import { relations } from "drizzle-orm";
-import { pgEnum, pgTable, text, uuid, timestamp, boolean, varchar } from "drizzle-orm/pg-core";
-
+import { pgEnum, pgTable, text, uuid, timestamp, boolean, varchar, integer } from "drizzle-orm/pg-core";
 
 export const statusEnum = pgEnum("status", ["ativo", "inativo"]);
 export const connectionStatusEnum = pgEnum("connection_status", ["open", "close", "connecting"]);
 
-// User
-export const userTable = pgTable("users", {
-    id: uuid("id").primaryKey().defaultRandom(),
-    name: varchar("name", { length: 255 }).notNull(),
-    email: varchar("email", { length: 255 }).notNull().unique(),
-    password: varchar("password", { length: 255 }).notNull(),
-    stripeCustomerId: text("stripe_customer_id"),
-    stripeSubscriptionId: text("stripe_subscription_id"),
-    status: statusEnum("status").notNull().default("inativo"),
-    createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
-    lastAccess: timestamp("last_access", { withTimezone: true }),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).notNull().defaultNow(),
+// Better Auth
+
+export const userTable = pgTable("user", {
+  id: uuid("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("email_verified").default(false).notNull(),
+  image: text("image"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  stripeCustomerId: text("stripe_customer_id"),
+  stripeSubscriptionId: text("stripe_subscription_id"),
+  status: statusEnum("status").notNull().default("inativo")
 });
+
+export const sessionTable = pgTable("session", {
+  id: uuid("id").primaryKey(),
+  expiresAt: timestamp("expires_at").notNull(),
+  token: text("token").notNull().unique(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+  ipAddress: text("ip_address"),
+  userAgent: text("user_agent"),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+});
+
+export const accountTable = pgTable("account", {
+  id: uuid("id").primaryKey(),
+  accountId: text("account_id").notNull(),
+  providerId: text("provider_id").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => userTable.id, { onDelete: "cascade" }),
+  accessToken: text("access_token"),
+  refreshToken: text("refresh_token"),
+  idToken: text("id_token"),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: text("scope"),
+  password: text("password"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
+
+export const verificationTable = pgTable("verification", {
+  id: uuid("id").primaryKey(),
+  identifier: text("identifier").notNull(),
+  value: text("value").notNull(),
+  expiresAt: timestamp("expires_at").notNull(),
+  createdAt: timestamp("created_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+  updatedAt: timestamp("updated_at").$defaultFn(
+    () => /* @__PURE__ */ new Date(),
+  ),
+});
+
+//  
+// My DATABASE
+//
+
+// // User
+// export const userTable = pgTable("users", {
+//     id: uuid("id").primaryKey().defaultRandom(),
+//     name: varchar("name", { length: 255 }).notNull(),
+//     email: varchar("email", { length: 255 }).notNull().unique(),
+//     password: varchar("password", { length: 255 }).notNull(),
+//     stripeCustomerId: text("stripe_customer_id"),
+//     stripeSubscriptionId: text("stripe_subscription_id"),
+//     status: statusEnum("status").notNull().default("inativo"),
+//     createdAt: timestamp("created_at", { withTimezone: true }).notNull().defaultNow(),
+//     lastAccess: timestamp("last_access", { withTimezone: true }),
+//     updatedAt: timestamp("updated_at", { withTimezone: true }).notNull().defaultNow(),
+// });
 
 export const userRelations = relations(userTable, ({ many }) => ({
     fluxes: many(fluxesTable),
